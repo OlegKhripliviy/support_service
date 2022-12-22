fd05e8bd318b1173f330f42ca66e155932ac0287
oleg <olegkhripliviy@gmail.com>
2022-12-22 17:23:59 +0200

    pre-commit fixed

--- src/users/models.py
+++ src/users/models.py
@@ -1,5 +1,5 @@
-from django.db import models
 from django.contrib.auth.models import AbstractBaseUser
+from django.db import models
 
 
 class User(AbstractBaseUser):
@@ -13,4 +13,3 @@ class User(AbstractBaseUser):
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
-
