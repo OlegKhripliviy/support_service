5a32744dd2156dc21562f3f8ad048e26b529d648
oleg <olegkhripliviy@gmail.com>
2022-12-26 11:59:17 +0200

    23.12.22

--- src/config/urls.py
+++ src/config/urls.py
@@ -1,5 +1,7 @@
+from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
     path("exchange-rates/", include("exchange_rates.urls")),
+    path("admin/", admin.site.urls),
 ]
--- src/users/models.py
+++ src/users/models.py
@@ -1,15 +1,35 @@
 from django.contrib.auth.models import AbstractBaseUser
 from django.db import models
 
+from users.managers import UserManager
+
 
 class User(AbstractBaseUser):
+    first_name = models.CharField(max_length=150, blank=True)
+    last_name = models.CharField(max_length=150, blank=True)
+    password = models.CharField(max_length=128)
     email = models.EmailField(unique=True)
-    password = models.CharField(max_length=100)
-    first_name = models.CharField(max_length=255)
-    last_name = models.CharField(max_length=100)
-    last_login = models.CharField(max_length=100)
-    activated = models.BooleanField()
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    role = models.CharField(max_length=10, choices=[])
+
+    objects = UserManager()
 
     EMAIL_FIELD = "email"
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
+
+    def clean(self):
+        super().clean()
+        self.email = self.__class__.objects.normalize_email(self.email)
+
+    def get_full_name(self):
+        """
+        Return the first_name plus the last_name, with a space in between.
+        """
+        full_name = "%s %s" % (self.first_name, self.last_name)
+        return full_name.strip()
+
+    def get_short_name(self):
+        """Return the short name for the user."""
+        return self.first_name
