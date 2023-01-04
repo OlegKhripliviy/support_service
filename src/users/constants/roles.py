1e02b60e8864edf2e1f2bd71ad900c9d14d351b8
oleg <olegkhripliviy@gmail.com>
2023-01-04 17:58:33 +0200

    update admin panel and models

--- src/users/constants/roles.py
+++ src/users/constants/roles.py
@@ -1,7 +1,17 @@
 from enum import Enum
+from functools import lru_cache
 
 
 class Role(str, Enum):
     ADMIN = "admin"
     USER = "user"
     MANAGER = "manager"
+
+    @classmethod
+    @lru_cache
+    def values(cls) -> list[tuple[str, str]]:
+        results = []
+        for element in cls:
+            el = (element.value, element.value.capitalize())
+            results.append(el)
+        return results
