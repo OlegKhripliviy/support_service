fd9edf9937b20d6371e84a7692b72eb2455db719
oleg <olegkhripliviy@gmail.com>
2023-01-04 23:21:46 +0200

    added comments, tickets, core

---
+++ src/comments/models.py
@@ -0,0 +1,13 @@
+from django.conf import settings
+from django.db import models
+
+from shared.django import TimeStampMixin
+from tickets.models import Ticket
+
+
+class Comment(TimeStampMixin):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.DO_NOTHING, related_name="comments"
+    )
+    body = models.TextField()
+    ticket = models.ForeignKey(Ticket, on_delete=models.CASCADE)
