b374ba527f01923a2a1e74bc1ea66ff9ff173167
oleg <olegkhripliviy@gmail.com>
2023-01-04 18:00:10 +0200

    code quality

--- src/users/admin.py
+++ src/users/admin.py
@@ -7,9 +7,14 @@ User = get_user_model()
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     exclude = ["user_permissions", "groups"]
-    readonly_fields = ["password", "last_login", "is_superuser", "is_staff", "is_active"]
+    readonly_fields = [
+        "password",
+        "last_login",
+        "is_superuser",
+        "is_staff",
+        "is_active",
+    ]
     list_display = ["email", "first_name", "last_name", "role", "is_active"]
     list_filter = ["role"]
     search_fields = ["email"]
     list_display_links = ["first_name"]
-
